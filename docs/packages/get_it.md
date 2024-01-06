48da7dd9b844d444d30d3a6d5a02de99bdd1471d
Stuard Ambrosio <boy.shut.in@gmail.com>
2024-01-06 11:16:28 -0500

    [12] Dependency Injection

---
+++ docs/packages/get_it.md
@@ -0,0 +1,11 @@
+# GET_IT
+
+## Introdución
+
+Este paquete es un servicio de localización de dependencias para Dart y Flutter. Permite registrar y obtener instancias de clases de forma sencilla.
+
+### Metodos
+
+- `registerFactory` : Registra una clase que se instanciará cada vez que se solicite.
+
+- `registerLazySingleton` : Registra una clase que se instanciará la primera vez que se solicite.
---
+++ lib/injector_container.dart
@@ -0,0 +1,51 @@
+import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'core/network/network_info.dart';
+import 'core/utils/input_converter.dart';
+import 'features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
+import 'features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+
+final getIt = GetIt.instance;
+
+void init() async {
+  //! BLOC
+  getIt.registerFactory(() => NumberTriviaBloc(
+      concrete: getIt(), random: getIt(), inputConverter: getIt()));
+
+  //! USESCASES
+  getIt.registerLazySingleton(
+      () => GetConcreteNumberTrivia(repository: getIt()));
+  getIt.registerLazySingleton(() => GetRandomNumberTrivia(repository: getIt()));
+
+  //! RESPOSITORY
+  getIt.registerLazySingleton<NumberTriviaRepository>(() =>
+      NumberTriviaRepositoryImpl(
+          networkInfo: getIt(),
+          localDataSource: getIt(),
+          remoteDataSource: getIt()));
+
+  //! DATA SOURCE
+  //! REMOTE
+  getIt.registerLazySingleton<NumberTriviaRemoteDataSource>(
+      () => NumberTriviaRemoteDataSourceImpl(client: getIt()));
+  //! LOCAL
+  getIt.registerLazySingleton<NumberTriviaLocalDataSource>(
+      () => NumberTriviaLocalDataSourceImpl(sharedPreferences: getIt()));
+
+  //! CORE
+  getIt.registerLazySingleton(() => InputConverter());
+  getIt.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(getIt()));
+
+  //! EXTERNAL
+  final sharedPreferences = await SharedPreferences.getInstance();
+  getIt.registerLazySingleton(() => sharedPreferences);
+  getIt.registerLazySingleton(() => http.Client());
+  getIt.registerLazySingleton(() => InternetConnectionChecker());
+}
